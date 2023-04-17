99213bf53c4e38d8604140a5a0a16eab335b6051
Mohamedamin Abdullahi <mohamedaminabdullahi@MohamedharsAir2.lan>
2023-04-17 12:41:53 -0500

    New changes to CSS file ;

--- src/Calender/index.js
+++ src/Calender/index.js
@@ -1,24 +1,80 @@
-import { DAYS } from "./conts"
-import { CalenderBody, CalenderHead, HeadDay, SevenColGrid, StyledDay, Wrapper } from "./styled"
-import {range} from "./utils";
+import { useState } from "react";
+import { DAYS } from "./conts";
+import { CalenderBody, CalenderHead, HeadDay, SevenColGrid, StyledDay, Wrapper } from "./styled";
+import { range } from "./utils";
 
-export const Calendar = () =>{
-    return <Wrapper>
-        <CalenderHead>
-        <ion-icon name="arrow-back-circle-outline"></ion-icon>
-        <p>Apr 2023</p>
-        <ion-icon name="arrow-forward-circle-outline"></ion-icon>
-        </CalenderHead>
-        <SevenColGrid>
-            {DAYS.map((day) => (
-                <HeadDay>{day}</HeadDay>
-            ))}
-        </SevenColGrid>
-            <CalenderBody>
-                {range(31).map((day) => (
-                <StyledDay>{day}</StyledDay>
-                ))}
-            </CalenderBody>
-        
+export const Calendar = () => {
+
+  // Initialize state variables for the current month and year
+  const today = new Date();
+  const [currentMonth, setCurrentMonth] = useState(today.getMonth());
+  const [currentYear, setCurrentYear] = useState(today.getFullYear());
+
+  // Event handler for the next month button and  to move to the next month when the "Next Month" button is clicked
+  const handleNextMonth = () => {
+    // If we're in December, go to the next year and the first month (January)
+    if (currentMonth === 11) {
+      setCurrentYear(currentYear + 1);
+      setCurrentMonth(0);
+    } else {
+
+      // Otherwise, just go to the next month
+      setCurrentMonth(currentMonth + 1);
+    }
+  };
+
+  // Render the header with the current month and year, and previous and next month buttons
+  const renderHeader = () => {
+    return (
+      <CalenderHead>
+        <button class="button" onClick={handleNextMonth}>
+          <ion-icon class="icon" name="arrow-back-circle-outline"></ion-icon>
+        </button>
+        <p>{`${new Date(currentYear, currentMonth, 1).toLocaleString("default", {
+          month: "long",
+        })} ${currentYear}`}</p>
+        <button class="button" onClick={handleNextMonth}>
+          <ion-icon class="icon" name="arrow-forward-circle-outline"></ion-icon>
+        </button>
+      </CalenderHead>
+    );
+  };
+
+  // Render the row of day of week labels (e.g. "Sun", "Mon", etc.)
+  const renderDaysOfWeek = () => {
+    return (
+      <SevenColGrid>
+        {DAYS.map((day) => (
+          <HeadDay>{day}</HeadDay>
+        ))}
+      </SevenColGrid>
+    );
+  };
+
+  // Render the days of the month as a grid, including any blank days at the start and end of the month
+  const renderDaysOfMonth = () => {
+    const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+    const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+    const blankDays = range(firstDayOfMonth );
+    return (
+      <CalenderBody>
+        {blankDays.map(() => (
+          <StyledDay></StyledDay>
+        ))}
+        {range(daysInMonth).map((day) => (
+          <StyledDay>{day}</StyledDay>
+        ))}
+      </CalenderBody>
+    );
+  };
+
+  // Render the entire calendar using the above helper functions
+  return (
+    <Wrapper>
+      {renderHeader()}
+      {renderDaysOfWeek()}
+      {renderDaysOfMonth()}
     </Wrapper>
-}
+  );
+};
+
--- src/Calender/utils.js
+++ src/Calender/utils.js
@@ -9,3 +9,4 @@ export const range = (end) =>{
 
 return result;
 };
+
