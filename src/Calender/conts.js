989bcf4b34e6b3a939a94f604f8ea125fb159fe4
Shuo Jiang <shuovok@gmail.com>
2023-04-25 16:24:18 -0500

    Adding comment

--- src/Calender/conts.js
+++ src/Calender/conts.js
@@ -1,7 +1,7 @@
 export const DAYS = ['Sun','Mon','Tue','Wed','Thur','Fri','Sat'];
 
 
-//Local date for event here:
+//data for all events:
 export const MOCKEVENTS = [
     { date: new Date(2023,3,23), title: 'appointment' },
     { date: new Date(2023,3,24), title: 'car wash'  },
