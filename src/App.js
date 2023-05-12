dfbf317145670c2c444ab1dc59682741982f49e2
Evan Rank <evanrank18@gmail.com>
2023-05-11 22:44:21 -0500

    updated app for backend integration

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
+
 import { useState } from "react";
 import { Calendar } from "./Calender";
-import { MOCKEVENTS } from "./Calender/conts";
 import "./styles.css";
 
+
 function App() {
-  const[events,setEvents] = useState(MOCKEVENTS);
+  const [events, setEvents] = useState([]);
 
-  const addEvent = (date,text) =>{
-    setEvents(prev => [...prev],{date,title: text});
+  const addEvent = (date, text) => {
+    const todo = { date, title: text };
 
-  }
-  return <div calssName="App">
-    <Calendar eventsArr={MOCKEVENTS} addEvent={addEvent}/>
-  </div>
+    fetch('/api/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(todo),
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log('Todo created successfully');
+          
+          setEvents(prev => [...prev, todo]);
+        } else {
+          console.error('Failed to create todo');
+        }
+      })
+      .catch(error => {
+        console.error('Error creating todo:', error);
+      });
+  };
+
+  return (
+    <div className="App">
+      <Calendar eventsArr={events} addEvent={addEvent} />
+    </div>
+  );
 }
 
 export default App;
